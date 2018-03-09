a71aa77409a7fc467822f7c4e7e57cec572b9548
Robert Sjoblom <robert.sjoblom@gmail.com>
2018-03-09 15:10:27 +0100

    migrating

---
+++ js/appState.js
@@ -0,0 +1,8 @@
+const appState = new Map();
+appState.set("templates", {
+    productTemplate: Handlebars.compile($("#product-template").html()),
+    reviewTemplate: Handlebars.compile($("#review-template").html()),
+    cartItemTemplate: Handlebars.compile($("#cart-item-template").html())
+}).set(
+    "cart", {}
+);
